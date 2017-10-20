19e8ab17950b92a70b867693dc96dc21c2c71435
dev001hajipro <dev001hajipro@gmail.com>
2017-10-21 08:14:01 +0900

    add to read funciton

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject study_clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "study clojure project."
+  :url "https://github.com/dev001hajipro/study_clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
--- src/study_clojure/core.clj
+++ src/study_clojure/core.clj
@@ -1,7 +1,20 @@
 (ns study-clojure.core
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
+(defn cwd []
+  print('hello'))
+
+; TODO exists
+; TODO open & write date
+; TODO delete file.
+
+(defn read-file [filepath]
+  (with-open [rdr (io/reader filepath)]
+    (doseq [str (line-seq rdr)]
+      (println str))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
+  (cwd)
   (println "Hello, World!"))
