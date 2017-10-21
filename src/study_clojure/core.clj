f79a327d46c3a24d5a432d74959f227d1612fedc
dev001hajipro <dev001hajipro@gmail.com>
2017-10-21 18:14:04 +0900

    append file/io code

--- src/study_clojure/core.clj
+++ src/study_clojure/core.clj
@@ -2,10 +2,6 @@
   (:require [clojure.java.io :as io])
   (:gen-class))
 
-(defn cwd []
-  print('hello'))
-
-; TODO exists
 ; TODO open & write date
 ; TODO delete file.
 
@@ -14,7 +10,31 @@
     (doseq [str (line-seq rdr)]
       (println str))))
 
+(defn read-file2 [filepath f]
+  (with-open [rdr (io/reader filepath)]
+    (doall (map f (line-seq rdr)))))
+
+(defn renew-file [filepath]
+  (if (.exists (io/as-file filepath))
+    (io/delete-file filepath))
+  (with-open [w (io/writer filepath)]
+    (.write w (str (now) "\n"))
+    (.write w (str "hellox"))))
+
+(defn now []
+  ; the dot of 'Date.' means instanciate 'now' in Java.
+  ; same like (new java.util.Date)
+  (java.util.Date.))       
+  
+
+  
+    
+(defn home-path []
+  (System/getProperty "user.home"))
+
 (defn -main
   [& args]
-  (cwd)
+  (home-path)
+  (read-file2 "c:\\tmp\\hello.json" print)
+  (renew-file "c:\\tmp\\hello_clojure.txt")
   (println "Hello, World!"))
