c6eeb3445bc3ff9ea55fee18e6e1459cace4b277
dev001hajipro <dev001hajipro@gmail.com>
2017-10-22 11:36:10 +0900

    add test for java.util.Date

--- src/study_clojure/core.clj
+++ src/study_clojure/core.clj
@@ -1,6 +1,24 @@
 (ns study-clojure.core
   (:require [clojure.java.io :as io])
+  (:import (java.util Date))
   (:gen-class))
+
+;;;;;;;;;;;;;;;;;;;;;;
+; util
+(defn now []
+  ; the dot of 'Date.' means instanciate 'now' in Java.
+  ; same like (new java.util.Date)
+  (Date.))
+    
+(defn home-path []
+  (System/getProperty "user.home"))
+
+(defmulti rand* (fn [min max] [(class min) (class max)]))
+(defmethod rand* [Long Long] [min max] 
+  (long (+ (rand (- (inc max) min)) min)))
+(defmethod rand* [Double Double] [min max] 
+  (+ (rand (- (inc max) min)) min))
+;;;;;;;;;;;;;;;;
 
 (defn read-file [filepath]
   (with-open [rdr (io/reader filepath)]
@@ -18,19 +36,10 @@
     (.write w (str (now) "\n"))
     (.write w (str "hellox"))))
 
-(defn now []
-  ; the dot of 'Date.' means instanciate 'now' in Java.
-  ; same like (new java.util.Date)
-  (java.util.Date.))
-    
-(defn home-path []
-  (System/getProperty "user.home"))
-
 (defn -main
   [& args]
   (home-path)
   (let [filepath "c:\\tmp\\hello_clojure.txt"]
     (renew-file filepath)
     (read-file2 filepath println))
-  
   (println "Hello, World!"))
--- test/study_clojure/core_test.clj
+++ test/study_clojure/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [study-clojure.core :refer :all]))
 
+; sample
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest now-test
+  (testing "current date object."
+    (is (instance? java.util.Date (now)))))
